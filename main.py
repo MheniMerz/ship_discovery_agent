09685a5d3e48ecc0952d0b28cf7a8a715ebdec82
mhenimerz <hani.merzouki@gmail.com>
2021-06-18 20:28:54 +0000

    fix typo

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client.load_system_host_keys()
 client.set_missing_host_key_policy(AutoAddPolicy())
 
 # read config file and foreach host create connection
-client.connect('border01', username='admin', password='YourPaSsWoRd!')
+client.connect('border01', username='admin', password='YourPaSsWoRd')
 stdin, stdout, stderr = client.exec_command('show arp')
 if stdout.channel.recv_exit_status() == 0:
     print(f'STDOUT: {stdout.read().decode("utf8")}')
