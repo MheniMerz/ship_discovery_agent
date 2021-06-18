2bd0f936cffd456778a188fe9bd30f94065bd55e
mhenimerz <hani.merzouki@gmail.com>
2021-06-18 00:58:13 +0000

    add ssh client template

---
+++ main.py
@@ -0,0 +1,17 @@
+from paramiko import SSHClient, AutoAddPolicy
+
+client = SSHClient()
+
+#load host ssh keys
+client.load_host_keys('~/.ssh/known_hosts')
+client.load_system_host_keys()
+
+# known_hosts policy
+client.set_missing_host_key_policy(AutoAddPolicy())
+
+# read config file and foreach host create connection
+
+#run commands
+# show arp
+# show ip route
+# show acl table, show acl rules
