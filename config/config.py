f891fc5eb2e4f0ce51db9fa8a1341b34fff00b78
mhenimerz <hani.merzouki@gmail.com>
2021-06-19 18:20:03 +0000

    Add config class

---
+++ config/config.py
@@ -0,0 +1,19 @@
+import os
+import configparser
+
+class Config:
+    def __init__(self):
+        self.conf_file_path = os.environ.get('CONF_FILE')
+        self.conf_file_contents = self.read_config()
+        
+    def read_config(self):
+        # open config file
+        try:
+            config = configparser.ConfigParser()
+            config.read(self.conf_file_path)
+            config.sections()
+            return config
+        except IOError:
+            log.critical("*********** ERROR reading config file **********")
+            exit(1)
+
