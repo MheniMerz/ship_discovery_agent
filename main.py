f5b6b2b85b4eecf37e37abfeee251bfe09486593
mhenimerz <hani.merzouki@gmail.com>
2021-06-21 13:30:25 +0000

    revert to one node

--- main.py
+++ main.py
@@ -13,22 +13,21 @@ client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
 client.set_missing_host_key_policy(AutoAddPolicy())
 
 # read config file and foreach host create connection
-for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
-    client.connect(
-            device, 
-            cfg.conf_file_contents['AUTH']['username'],
-            cfg.conf_file_contents['AUTH']['password'])
-    #run the command
-    stdin, stdout, stderr = client.exec_command('show arp')
-    if stdout.channel.recv_exit_status() == 0:
-        print(f'{stdout.read().decode("utf8")}')
-    else:
-        print('===================================')
-        print(f'{stderr.read().decode("utf8")}')
-        print('===================================')
-    stdin.close()
-    stdout.close()
-    stderr.close()
+client.connect(
+        'border01', 
+        username='admin',
+        password='YourPaSsWoRd')
+#run the command
+stdin, stdout, stderr = client.exec_command('show arp')
+if stdout.channel.recv_exit_status() == 0:
+    print(f'{stdout.read().decode("utf8")}')
+else:
+    print('===================================')
+    print(f'{stderr.read().decode("utf8")}')
+    print('===================================')
+stdin.close()
+stdout.close()
+stderr.close()
 
 client.close()
 #run commands
