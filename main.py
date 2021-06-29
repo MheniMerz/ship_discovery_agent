bac5728a0de723309edb945591a6f0067d191ebd
Shane <shane10j@gmail.com>
2021-06-29 12:45:54 -0400

    checking config.ini

--- main.py
+++ main.py
@@ -12,20 +12,16 @@ commandList = ['show arp', 'show ip route', 'show acl table', 'show acl rule']
 
 # load host ssh keys
 client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
-print("1")
 
 # known_hosts policy
 client.set_missing_host_key_policy(AutoAddPolicy())
-print("2")
 
 # read config file and foreach host create connection
 for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
-    print("3")
     client.connect(
         device,
-        username=str(cfg.conf_file_contents['AUTH']['username']),
-        password=str(cfg.conf_file_contents['AUTH']['password']))
-    print("4")
+        username=cfg.conf_file_contents['AUTH']['username'],
+        password=cfg.conf_file_contents['AUTH']['password'])
     for i in commandList:
         print('\n' + device + ': ' + i.split(' ', 1)[1] + '\n')
         stdin, stdout, stderr = client.exec_command(i)
