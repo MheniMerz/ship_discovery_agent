ee7f3283a6a86fb259fa41bacfe3ee1d43ba97ee
mhenimerz <hani.merzouki@gmail.com>
2021-07-04 16:10:25 +0000

    fix relative file path

--- parser/parser.py
+++ parser/parser.py
@@ -1,8 +1,10 @@
 import textfsm
+import os
 
 class Parser:
     def parse_show_arp(self, query_result: str):
-        with open('templates/show_arp.template') as template:
+        template_path = os.path.join(os.path.dirname(__file__),'templates/show_arp.template')
+        with open(template_path) as template:
             fsm = textfsm.TextFSM(template)
             result = fsm.ParseText(query_result)
             return result
