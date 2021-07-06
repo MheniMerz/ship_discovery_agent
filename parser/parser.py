f32d915910a45685d6ed114c4161f573e121b84a
mhenimerz <hani.merzouki@gmail.com>
2021-07-06 22:41:22 +0000

    parse to JSON

--- parser/parser.py
+++ parser/parser.py
@@ -1,12 +1,24 @@
 import textfsm
 import os
+import .query.query
 
 class Parser:
+    def __init__(self):
+        self.headers = []
+        self.data = []
+        self.json_data = {}
+
     def parse_show_arp(self, query_result: str):
         template_path = os.path.join(os.path.dirname(__file__),'templates/show_arp.template')
         with open(template_path) as template:
             fsm = textfsm.TextFSM(template)
-            result = fsm.ParseText(query_result)
-            return result
+            self.headers = fsm.header
+            self.data = fsm.ParseText(query_result)
+            self.parse_to_json()
+            return self.json_data
 
+    def parse_to_json(self):
+        json_dict['columns'] = self.headers
+        json_dict['rows'] = self.data
+        self.json_data = json.loads(json_dict)
 
