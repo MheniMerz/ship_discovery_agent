955bd8ca117eacdd7096d203cdf432a2abee83b0
Shane <shane10j@gmail.com>
2021-07-25 20:12:38 -0400

    test

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ jsonDict = {}
 n = 0
 
 # list of commands that will be run for each node on network
-commandList = ['show arp', 'show ip route', 'show acl table', 'show acl rule', 'show lldp table', 'show vlan config',
-               'vtysh -c "show interface"', 'show ip bgp neighbors']
+commandList = ['show arp']
 headerList = ['arp', 'ipRoute', 'aclTable', 'aclRule', 'lldp', 'vlan', 'interface', 'bgp']
 
 # load host ssh keys
