2bd02a6fe1257301993a68e28d82a7d2f97aa8f8
Shane <shane10j@gmail.com>
2021-07-25 20:03:06 -0400

    test

--- main.py
+++ main.py
@@ -39,7 +39,8 @@ for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
         current_query = Query(device, i)
         current_query.send_query(client)
         query_dictionary[current_query.device + '.' + current_query.cmd] = current_query
-        print(current_query)
+        if i == 0:
+            print(current_query)
 client.close()
 
 for i in query_dictionary:
