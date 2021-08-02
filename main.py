d1acaff0ad7f4f6151dd3521787bbeeb588b77d7
Shane <shane10j@gmail.com>
2021-08-02 18:42:45 -0400

    test

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ jsonFile.close()
 # sending the json file to emulated controller
 url = "https://127.0.0.1:5000/about"
 
-headers = CaseInsensitiveDict()
+headers = {}
 headers["Content-Type"] = "application/json"
 
 response = requests.post(url, headers=headers, data=jsonFile)
