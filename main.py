653cf92a198a5345fb43985edaefc5312a3a6752
Shane <shane10j@gmail.com>
2021-08-03 10:47:30 -0400

    test

--- main.py
+++ main.py
@@ -59,8 +59,8 @@ jsonFile.write(json_network)
 # sending the json file to emulated controller
 url = "https://127.0.0.1:5000/upload"
 
-headers = {}
+files = {'file': open('jsonFile.json', 'rb')}
 
-response = requests.post(url, headers=headers, data=jsonFile)
+response = requests.post(url, file=file)
 jsonFile.close()
 #test
