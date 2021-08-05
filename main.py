6783964722b3674a930a2a0291931c02270807e9
Shane <shane10j@gmail.com>
2021-08-04 21:23:56 -0400

    Test

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ json_network = json.dumps(jsonDict)
 jsonFile = open("data.json", "w+")
 jsonFile.write(json_network)
 
-def retry_on_connectionerror(f, max_retries=5):
+def retry_on_connectionerror(f, max_retries=10):
   retries = 0
   while retries < max_retries:
     try:
@@ -68,7 +68,7 @@ def retry_on_connectionerror(f, max_retries=5):
 # sending the json file to emulated controller
 def sendJSON():
     url = 'http://127.0.0.1:5000/upload'
-    filedata = open('data.json', 'r')
+    filedata = open('data.json', 'rpdb')
     #filedata = {'file': open("data.json", "rb")}
     #filedata = {'file': ('data.json', open('data.json', 'rb'))}
     #headers = {'Content-type': 'application/json', 'Accept' : 'application/json'}
