b4e136bee35636cf48e05fa10f7f16266fe0a19b
Shane <shane10j@gmail.com>
2021-08-05 16:47:21 -0400

    test

--- jsonSend.py
+++ jsonSend.py
@@ -1,6 +1,7 @@
 import requests
 
-def postRequest(self, url, filename):
-    filedata = {'filedata': (filename, open(filename, 'rb'))}
-    response = requests.post(url, files=filedata)
-    return response.text
+class jsonSend:
+    def postRequest(self, url, filename):
+        filedata = {'filedata': (filename, open(filename, 'rb'))}
+        response = requests.post(url, files=filedata)
+        return response.text
