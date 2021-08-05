d0dedf0dc6177bbd20f923a68bdc9fbaaac22a64
Shane <shane10j@gmail.com>
2021-08-05 18:47:58 -0400

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from paramiko import SSHClient, AutoAddPolicy
 from config.config import Config
 from query.query import Query
 from parser.parser import Parser
-import jsonSend
+from jsonSend import * 
 import collections
 import requests
 from requests.exceptions import ConnectionError
