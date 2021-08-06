2246b54cf3e7fefdfa761f5e46edc34522ff3383
Shane <shane10j@gmail.com>
2021-08-06 13:35:25 -0400

    Test

--- main.py
+++ main.py
@@ -25,42 +25,62 @@ commandList = ['show arp', 'show ip route', 'show acl table', 'show acl rule', '
                'vtysh -c "show interface"', 'show ip bgp neighbors']
 headerList = ['arp', 'ipRoute', 'aclTable', 'aclRule', 'lldp', 'vlan', 'interface', 'bgp']
 
-# load host ssh keys
-client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+def loadSSH():
+    # load host ssh keys
+    client.load_host_keys(os.path.expanduser('~/.ssh/known_hosts'))
+    # known_hosts policy
+    client.set_missing_host_key_policy(AutoAddPolicy())
 
-# known_hosts policy
-client.set_missing_host_key_policy(AutoAddPolicy())
+def collectData():
+    # read config file and foreach host create connection
+    for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
+        client.connect(
+            device,
+            username=cfg.conf_file_contents['AUTH']['username'],
+            password=cfg.conf_file_contents['AUTH']['password'])
+        deviceList.append(device)
+        for i in commandList:
+            current_query = Query(device, i)
+            current_query.send_query(client)
+            query_dictionary[current_query.device + '.' + current_query.cmd] = current_query
+    client.close()
 
-# read config file and foreach host create connection
-for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
-    client.connect(
-        device,
-        username=cfg.conf_file_contents['AUTH']['username'],
-        password=cfg.conf_file_contents['AUTH']['password'])
-    deviceList.append(device)
-    for i in commandList:
-        current_query = Query(device, i)
-        current_query.send_query(client)
-        query_dictionary[current_query.device + '.' + current_query.cmd] = current_query
-client.close()
+def jsonParse():
+    # parsing data into JSON
+    for i in query_dictionary:
+        result = parser.parse_query_result(query_dictionary[i])
+        outputDict[headerList[n % len(headerList)]] = result
+        if ((n+1) % len(headerList)) == 0:
+            jsonDict[deviceList[int(n / len(headerList))]] = outputDict
+            outputDict = {}
+        n += 1
+    json_network = json.dumps(jsonDict)
 
-# parsing data into JSON
-for i in query_dictionary:
-    result = parser.parse_query_result(query_dictionary[i])
-    outputDict[headerList[n % len(headerList)]] = result
-    if ((n+1) % len(headerList)) == 0:
-        jsonDict[deviceList[int(n / len(headerList))]] = outputDict
-        outputDict = {}
-    n += 1
-json_network = json.dumps(jsonDict)
+    # saving JSON output to a JSON file
+    jsonFile = open("data.json", "w+")
+    jsonFile.write(json_network)
 
-# saving JSON output to a JSON file
-jsonFile = open("data.json", "w+")
-jsonFile.write(json_network)
+def jsonSend():
+    url = 'http://127.0.0.1:5000/upload'
+    filename = 'data.json'
 
-# uploading JSON file to controller
-url = 'http://127.0.0.1:5000/upload'
-filename = 'data.json'
-current_request = Request()
-current_request.postRequest(url, filename)
-jsonFile.close()
+    # uploading JSON file to controller
+    current_request = Request()
+    current_request.postRequest(url, filename)
+    jsonFile.close()
+
+if __name__ == '__main__':
+    conf = Config()
+    #load ssh keys and set up known_hosts
+    loadSSH()
+    if(conf.repeat_timer == None):
+        collectData()
+        jsonParse()
+        jsonSend()
+    else:
+        while(True):
+            collectData()
+            jsonParse()
+            jsonSend()
+            time.sleep(int(conf.repeat_timer))
+        
