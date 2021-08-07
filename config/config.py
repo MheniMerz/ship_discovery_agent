ed7b110d0202c0905eaa8bd017b52869d5bfcc70
Shane <shane10j@gmail.com>
2021-08-07 19:03:04 -0400

    Test

--- config/config.py
+++ config/config.py
@@ -4,7 +4,7 @@ import configparser
 
 class Config:
     def __init__(self):
-        repeat_timer = os.environ.get('REPEAT_TIMER')
+        self.repeat_timer = os.environ.get('REPEAT_TIMER')
         self.conf_file_path = os.environ.get('CONF_FILE')
         self.conf_file_contents = self.read_config()
 
