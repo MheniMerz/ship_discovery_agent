f8ddc76641519801dc3151f9780771efe6b84f3e
Shane <shane10j@gmail.com>
2021-08-08 17:52:15 -0400

    test

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ import configparser
 from dotenv import load_dotenv
 from pathlib import Path
 
-dotenv_path = Path('/ship_discovery_agent/config/env_vars.env')
+dotenv_path = Path('/ship_discovery_agent/env_vars.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 class Config:
