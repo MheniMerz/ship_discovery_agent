2f5c3c3fd5bc02cd5ee9594e1901fa2b37fa755d
Shane <shane10j@gmail.com>
2021-08-08 18:16:26 -0400

    test

--- init.sh
+++ init.sh
@@ -3,5 +3,5 @@
 touch ~/.ssh/known_hosts
 
 #add environment variables to the bashrc
-awk '{print "export "$0}' config/env_vars.env >> ~/.bashrc
+awk '{print "export " $0}' config/env_vars.env >> ~/.bashrc
 . ~/.bashrc
