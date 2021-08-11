3e9c45221cde5318e40ec0922c962f96967d8fee
Shane <shane10j@gmail.com>
2021-08-11 18:50:54 -0400

    test

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def loadSSH():
     #direcList = glob.glob("/*")
     #for direc in direcList:
         #print(glob.glob(direc + "/*"))
-    print(cfg.conf_file_path))
+    print(cfg.conf_file_path)
 def collectData():
     # read config file and foreach host create connection
     config = configparser.ConfigParser()
