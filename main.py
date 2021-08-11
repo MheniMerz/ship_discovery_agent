4ba075925987e94e53a25e7ae153d0c421259826
Shane <shane10j@gmail.com>
2021-08-11 18:15:42 -0400

    test

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def collectData():
     # read config file and foreach host create connection
     config = configparser.ConfigParser()
     config.read("/ship_discovery_agent/config/config.ini")
-    print(config['TARGETS']['devices'])
+    #print(config['TARGETS']['devices'])
     for device in json.loads(cfg.conf_file_contents['TARGETS']['devices']):
         client.connect(
             device,
